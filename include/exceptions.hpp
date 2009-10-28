f7aad3da99a904fd5517e536b4afff6d0632e944
joxer <kell.92.k@gmail.com>
2009-10-28 22:18:03 +0100

    penis

---
+++ include/exceptions.hpp
@@ -0,0 +1,12 @@
+#include <exception>
+#include <iostream>
+class ValueNotDefinited: public std::exception {
+
+
+public:
+  
+  void reason();
+
+};
+
+
---
+++ src/exceptions.cpp
@@ -0,0 +1,6 @@
+#ifndef EXCEPTIONS_HPP
+#include "../include/exceptions.hpp"
+#endif
+void ValueNotDefinited::reason(){
+  std::cout << "Value of height or width not definited\n";
+}
